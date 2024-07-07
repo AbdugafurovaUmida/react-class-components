a64a77830668182b9178d186f2f2dae67099198b
AbdugafurovaUmida <abdugafurova93@gmail.com>
2024-07-07 21:39:42 +0500

    fix: change app file, call components, add localstorage functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,55 @@
-import React from 'react'
+import { Component } from 'react'
+import Search from './components/search/Search'
+import Results from './components/result/Results'
+import * as React from 'react'
+import { SEARCH } from './consts/index'
+import { getPeoples } from './api/people/index'
+import ResponseApi from './types/api'
+import People from 'types/people'
+import ErrorBoundary from './components/error-boundary/ErrorBoundary'
 
-class App extends React.Component {
-  state = {
-    cards: [],
+type State = {
+  defaultValue: string
+  isLoading: boolean
+  response?: ResponseApi<People>
+}
+
+class App extends Component<object, State> {
+  state: State = { defaultValue: '', isLoading: false }
+
+  componentDidMount() {
+    const defaultValue = localStorage.getItem(SEARCH) || ''
+    this.setState({ defaultValue, isLoading: true })
+    getPeoples(defaultValue).then((response) => this.setState({ response, isLoading: false }))
   }
 
-  async componentDidMount(): Promise<void> {}
+  handleChange = async (value: string) => {
+    localStorage.setItem(SEARCH, value)
+    this.setState({ isLoading: true, response: undefined })
+    const response = await getPeoples(value)
+    this.setState({ response, isLoading: false })
+  }
 
-  render(): React.ReactNode {
+  render() {
     return (
-      <>
-        <div>hi</div>
-      </>
+      <ErrorBoundary fallback={<div>Opsss, something went wrong</div>}>
+        <div className='container'>
+          <div className='home'>
+            <section className='section-search'>
+              <div>Type name hero from Star war</div>
+              <Search
+                isLoading={this.state.isLoading}
+                defaultValue={this.state.defaultValue}
+                onChange={this.handleChange}
+              />
+            </section>
+
+            <hr />
+
+            <Results isLoading={this.state.isLoading} data={this.state.response} />
+          </div>
+        </div>
+      </ErrorBoundary>
     )
   }
 }
