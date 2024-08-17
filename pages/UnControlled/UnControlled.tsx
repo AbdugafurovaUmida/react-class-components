ca41a5fbe43eb9f2b9dc9677212828de2129d72c
AbdugafurovaUmida <abdugafurova93@gmail.com>
2024-08-17 18:11:33 +0500

    feat: implement routing

---
+++ pages/UnControlled/UnControlled.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UnControlled = () => {
+  return <div>UnControlled</div>;
+};
+
+export default UnControlled;
--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import Main from '../pages/Main/MainPage';
+import Controlled from '../pages/Controlled/Controlled';
+import UnControlled from '../pages/UnControlled/UnControlled';
+import NotFound from '../pages/NotFound/NotFound';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ErrorElement } from './shared/utils/errorBoundary/ErrorElement';
 
-function App() {
-  const [count, setCount] = useState(0);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+    errorElement: <ErrorElement />,
+  },
+  {
+    path: 'controlled',
+    element: <Controlled />,
+  },
+  {
+    path: 'uncontrolled',
+    element: <UnControlled />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
-  );
+export default function App() {
+  return <RouterProvider router={router} />;
 }
-
-export default App;
